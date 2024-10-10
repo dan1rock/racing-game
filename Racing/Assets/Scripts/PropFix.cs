c10ccb939fcd3d2e1d3619f5d27062f26d6109a0
Danylo Holets <90461399+dan1rock@users.noreply.github.com>
2024-10-10 21:31:04 +0300

    Decorated new track

--- Racing/Assets/Scripts/PropFix.cs
+++ Racing/Assets/Scripts/PropFix.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PropFix : MonoBehaviour
 {
-    private void Awake()
+    [ContextMenu("Fix This")]
+    public void Fix()
     {
         Ray ray = new()
         {
@@ -17,9 +18,24 @@ public class PropFix : MonoBehaviour
             
             const float maxAngle = 10f * Mathf.Deg2Rad;
 
+            Quaternion currentRotation = transform.rotation;
+            
             Vector3 limitedUp = Vector3.RotateTowards(Vector3.up, hitInfo.normal, maxAngle, 0f);
             
-            transform.up = limitedUp;
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, limitedUp);
+            
+            transform.rotation = Quaternion.Euler(currentRotation.eulerAngles.x, currentRotation.eulerAngles.y, targetRotation.eulerAngles.z);
+        }
+    }
+
+    [ContextMenu("Fix All")]
+    public void FixAll()
+    {
+        PropFix[] propFixes = FindObjectsOfType<PropFix>();
+
+        foreach (PropFix propFix in propFixes)
+        {
+            propFix.Fix();
         }
     }
 }

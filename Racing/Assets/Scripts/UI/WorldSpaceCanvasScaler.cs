c16f9748993136cbe12b015c72a6bbcaf1cbce6d
Danylo Holets <90461399+dan1rock@users.noreply.github.com>
2024-10-23 19:52:16 +0300

    Improved canvas adjustment

--- Racing/Assets/Scripts/UI/WorldSpaceCanvasScaler.cs
+++ Racing/Assets/Scripts/UI/WorldSpaceCanvasScaler.cs
@@ -1,12 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldSpaceCanvasScaler : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
+    [SerializeField] private List<Transform> objectsToAdjust;
+    [SerializeField] private float borderlineAspect = 5f / 3f;
     [SerializeField] private bool lockMaxAspect;
 
+    private List<Vector3> _adjustableObjectsDefaultScales = new();
+    
     private Vector2 _defaultResolution;
     private float _defaultAspect;
+
+    private float _recordedAspect;
 
     private RectTransform _canvasRectTransform;
 
@@ -22,6 +29,11 @@ public class WorldSpaceCanvasScaler : MonoBehaviour
         _defaultResolution = new Vector2(_canvasRectTransform.rect.width, _canvasRectTransform.rect.height);
         _defaultAspect = _defaultResolution.x / _defaultResolution.y;
 
+        foreach (Transform t in objectsToAdjust)
+        {
+            _adjustableObjectsDefaultScales.Add(t.localScale);
+        }
+        
         AdjustCanvasToScreen();
     }
 
@@ -34,6 +46,8 @@ public class WorldSpaceCanvasScaler : MonoBehaviour
     {
         float screenAspect = (float)Screen.width / (float)Screen.height;
         
+        if (screenAspect == _recordedAspect) return;
+        
         float scaleFactor = screenAspect / _defaultAspect;
 
         if (lockMaxAspect && scaleFactor > 1f) scaleFactor = 1f;
@@ -41,5 +55,14 @@ public class WorldSpaceCanvasScaler : MonoBehaviour
         float newWidth = _defaultResolution.x * scaleFactor;
         
         _canvasRectTransform.sizeDelta = new Vector2(newWidth, _defaultResolution.y);
+
+        bool adjust = screenAspect < borderlineAspect;
+        float adjustment = adjust ? screenAspect / borderlineAspect : 1f;
+        for (int i = 0; i < objectsToAdjust.Count; i++)
+        {
+            objectsToAdjust[i].localScale = _adjustableObjectsDefaultScales[i] * adjustment;
+        }
+
+        _recordedAspect = screenAspect;
     }
 }
